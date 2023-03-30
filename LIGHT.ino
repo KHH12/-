4667e16f23fde4b228cbfb85b60abf7d45287751
KHH12 <129159887+KHH12@users.noreply.github.com>
2023-03-30 11:07:42 +0900

    Add files via upload

---
+++ LIGHT.ino
@@ -0,0 +1,16 @@
+const int LED = 13;
+
+void setup(){
+  pinMode(LED,OUTPUT);
+  //digitalWrite(LED, HIGH);
+  //digitalWrite(LED, LOW);
+}
+
+void loop() {
+for (int t_high=0; t_high<=10; t_high++){
+  digitalWrite(LED, HIGH);
+  delay(t_high);
+  digitalWrite(LED, LOW);
+  delay(10-t_high);
+}
+
